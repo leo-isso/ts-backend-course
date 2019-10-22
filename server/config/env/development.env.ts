d285f0ce52b7d257c119208ab33df200d91e0207
Leonardo Isso <leoisso.work@gmail.com>
2019-10-21 21:32:10 -0300

    Add env configs

---
+++ server/config/env/development.env.ts
@@ -0,0 +1,12 @@
+module.exports = {
+    env: "deveplopment",
+    db: "ts-api",
+    dialet: "postgres",
+    username: "postgres",
+    password: "leoisso",
+    host: "localhost",
+    serverPort: 3000,
+    pgPort: 5432,
+    dbUrl: "http://159.203.177.102:5432/ts-api",
+    secret: "secret"
+}
---
+++ server/config/env/test.env.ts
@@ -0,0 +1,12 @@
+module.exports = {
+    env: "test",
+    db: "ts-api-test",
+    dialet: "postgres",
+    username: "postgres",
+    password: "leoisso",
+    host: "localhost",
+    serverPort: 3000,
+    pgPort: 5432,
+    dbUrl: "http://159.203.177.102:5432/ts-api-test",
+    secret: "secret"
+}
