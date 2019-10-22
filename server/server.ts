b563e45570f81c01efc32d65482788cc5b3f9df0
Leonardo Isso <leoisso.work@gmail.com>
2019-10-21 21:49:57 -0300

    Fix listeners

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,10 @@
 import * as http from "http";
 import Api from './api/api';
-import { errorHandlerApi } from "./api/errorHandlerApi";
 
 const config = require("./config/env/config")();
 
 const server = http.createServer(Api);
 
-Api.use(errorHandlerApi);
-
-server.listen(config.serverPort, () => console.log(`Running server @ port ${config.serverPort}.`))
+server.listen(config.serverPort)
+server.on("listening", () => console.log(`Server running @ port ${config.serverPort}`))
+server.on("error", (error: NodeJS.ErrnoException) => console.log(`Found an error: ${error}`))
